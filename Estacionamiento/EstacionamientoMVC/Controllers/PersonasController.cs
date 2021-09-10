6a020760a1b7699e1d6388712e92951bb7f265bb
marianolongoort <mariano.longo@ort.edu.ar>
2021-09-09 22:42:28 -0300

    2021.0909

---
+++ Estacionamiento/EstacionamientoMVC/Controllers/PersonasController.cs
@@ -0,0 +1,80 @@
+﻿using EstacionamientoMVC.Data;
+using EstacionamientoMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EstacionamientoMVC.Controllers
+{
+    public class PersonasController : Controller
+    {
+        private EstacionamientoContext cheMiContexto;
+            
+
+        public PersonasController(EstacionamientoContext contexto)
+        {
+            this.cheMiContexto = contexto;
+        }
+
+
+        public IActionResult Index()
+        {
+            Persona persona1 = new Persona() { Nombre = "Pedro", Apellido = "Picapiedra" };
+            Persona persona2 = new Persona() { Nombre = "Pablo", Apellido = "Marmol" };
+            Persona persona3 = new Persona() { Nombre = "Vilma", Apellido = "Picapiedra" };
+
+            List<Persona>         listaPersonas = new List<Persona>();
+            IEnumerable<Persona>  ienumPersonas = new List<Persona>();
+            ICollection<Persona>  icolPersonas  = new List<Persona>();
+            IList<Persona>        ilistPersonas = new List<Persona>();
+
+            icolPersonas.Add(persona1);
+            icolPersonas.Add(persona2);
+
+            listaPersonas.AddRange(icolPersonas);
+
+
+            ViewBag.Supervisor = persona3;
+            
+
+
+            return View(listaPersonas);
+        }
+
+
+        public IActionResult ListarPersonas()
+        {
+            
+            bool hayPersonas = cheMiContexto.Personas.Any(per => per.Nombre == "Pablo");
+
+            List<Persona> losPedros = cheMiContexto.Personas.Where(per => per.Nombre == "Pedro").ToList();
+            List<Persona> losPablos = cheMiContexto.Personas.Where(per => per.Nombre == "Pablo").ToList();
+
+            if (!hayPersonas)
+            {
+                Persona persona1 = new Persona()
+                {
+                    Nombre = "Pablo",
+                    Apellido = "Marmol",
+                    Email = "pablo@marmol.com",
+                    DNI = 22333444
+                };
+
+                cheMiContexto.Personas.Add(persona1);
+                cheMiContexto.SaveChanges();
+            }
+
+            ViewBag.LosPedros = losPedros;
+            ViewBag.LosPablos = losPablos;
+
+          
+
+            List<Persona> personasEnBaseDeDAtos = cheMiContexto.Personas.ToList();
+
+            return View(personasEnBaseDeDAtos);
+        }
+
+    }
+}
